bd1de445af282b1c4975d6b3a513957f4c5c92c1
Guilherme-07062002 <guilherme762002@gmail.com>
2024-05-15 22:25:46 -0300

    refactor: add created_at and updated_at fields to Task model

--- todolist/models.py
+++ todolist/models.py
@@ -3,10 +3,13 @@ from django.db import models
 class Task(models.Model):
     task_text = models.CharField(max_length=200)
     task_status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.task_text
     
     def is_done(self):
         return self.task_status
+        
         
