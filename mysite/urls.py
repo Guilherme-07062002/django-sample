521061398232d45a95083c47a9ee64124e5642da
Guilherme-07062002 <guilherme762002@gmail.com>
2024-05-15 21:28:45 -0300

    test: first automated test

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    path("polls/", include(("polls.urls", "polls"), namespace="polls")),
     path("admin/", admin.site.urls),
 ]
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class PollsViewsTestCase(TestCase):
+    def test_index(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello, world. You're at the polls index.")
+    
