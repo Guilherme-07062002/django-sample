788c5f21f14ea19ac0fdd5214359c51da2755d10
Guilherme-07062002 <guilherme762002@gmail.com>
2024-05-15 21:29:40 -0300

    test: add tests for detail, results, and vote views

--- polls/tests.py
+++ polls/tests.py
@@ -6,4 +6,19 @@ class PollsViewsTestCase(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Hello, world. You're at the polls index.")
+
+    def test_detail(self):
+        response = self.client.get(reverse('polls:detail', args=(1,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You're looking at question 1.")
+    
+    def test_results(self):
+        response = self.client.get(reverse('polls:results', args=(1,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You're looking at the results of question 1.")
+    
+    def test_vote(self):
+        response = self.client.get(reverse('polls:vote', args=(1,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You're voting on question 1.")
     
