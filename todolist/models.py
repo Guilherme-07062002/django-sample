2be6d8a3c3419d4231188e0326733c90fce437c6
Guilherme-07062002 <guilherme762002@gmail.com>
2024-05-15 21:54:02 -0300

    feat: task model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    task_text = models.CharField(max_length=200)
+    task_status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.task_text
+    
+    def is_done(self):
+        return self.task_status
+        
